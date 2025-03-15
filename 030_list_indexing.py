74017dfb5dc5c98f2f622f1e90122ce4f15c88ed
zohair-1995 <zohair.abidi1995@outlook.com>
2025-03-15 11:02:09 +0000

    feat(030_list_indexing.py): completed all the implementations for all the functions found within each exercises 1, 2, 3, and 4 respectively

--- 030_list_indexing.py
+++ 030_list_indexing.py
@@ -23,7 +23,8 @@ print("Function: get_first_item")
 
 def get_first_item(the_list):
   # Return the first item of the list
-  pass
+  #pass
+  return the_list[0]
 
 check_that_these_are_equal(
   get_first_item(["a", "b", "c", "d", "e"]),
@@ -42,7 +43,8 @@ print("Function: get_last_item")
 
 def get_last_item(the_list):
   # Return the last item of the list
-  pass
+  #pass
+  return the_list[-1]
 
 check_that_these_are_equal(
   get_last_item(["a", "b", "c", "d", "e"]),
@@ -61,7 +63,8 @@ print("Function: get_nth_item")
 
 def get_nth_item(the_list, n):
   # Return the item of the list at the specified index
-  pass
+  #pass
+  return the_list[n]
 
 check_that_these_are_equal(
   get_nth_item(["a", "b", "c", "d", "e"], 3),
@@ -80,7 +83,8 @@ print("Function: get_items_between_one_and_three")
 
 def get_items_between_one_and_three(the_list):
   # Return the section of the list between indexes one and three
-  pass
+  #pass
+  return the_list[1:3]
 
 check_that_these_are_equal(
   get_items_between_one_and_three(["a", "b", "c", "d", "e"]),
