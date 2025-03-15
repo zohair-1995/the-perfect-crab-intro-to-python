bb0bde6f69679cf1d2b38aa1e66c72ca75f9329c
zohair-1995 <zohair.abidi1995@outlook.com>
2025-03-15 12:34:11 +0000

    feat(032_while_loops.py): completed the implementation for the function add_cats_repeatedly

--- 032_while_loops.py
+++ 032_while_loops.py
@@ -45,7 +45,10 @@ print("Function: add_cats_repeatedly")
 # => ['cats', 'cats', 'cats']
 
 def add_cats_repeatedly(word_list, count):
-  # ...
+  i = 0
+  while i != count:
+    word_list.append("cats")
+    i += 1
   return word_list
 
 check_that_these_are_equal(
