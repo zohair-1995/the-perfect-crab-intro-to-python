dfb29020a06fa64224b7b3a0185025d13587797d
zohair-1995 <zohair.abidi1995@outlook.com>
2025-03-15 09:58:16 +0000

    feat(026_ifs.py): completed the implementation for the functions is_first_of_the_month and has_five_chars

--- 026_ifs.py
+++ 026_ifs.py
@@ -49,7 +49,11 @@ print("Function: is_first_of_the_month")
 def is_first_of_the_month(day_number):
   # Return "First of the month!" if the day number is 1. Return "Not first of
   # the month" otherwise.
-  pass
+  #pass
+  if day_number == 1:
+    return "First of the month!"
+  else:
+    return "Not first of the month"
 
 check_that_these_are_equal(
   is_first_of_the_month(1),
@@ -69,7 +73,11 @@ print("Function: has_five_chars")
 def has_five_chars(the_str):
   # Return "STRING is five characters long" if the string is five characters
   # long. Otherwise, return "Not five characters".
-  pass
+  #pass
+  if len(the_str) == 5:
+    return f"{the_str} is five characters long"
+  else:
+    return "Not five characters"
 
 check_that_these_are_equal(
   has_five_chars("ABCDE"),
